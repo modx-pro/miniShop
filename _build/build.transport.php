ff2fa800b360caa99732c40a0194be103bdcc02a
bezumkin <bezumkin@yandex.ru>
2012-02-28 16:22:33 +0400

    Version 1.0.2 beta1

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -34,8 +34,8 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','miniShop');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('PKG_VERSION','1.0.1');
-define('PKG_RELEASE','beta2');
+define('PKG_VERSION','1.0.2');
+define('PKG_RELEASE','beta1');
 
 /* define sources */
 $root = dirname(dirname(__FILE__)).'/';
