74c679b73cbbad1e463ac711b4f6647edef71335
Bezumkin <bezumkin@yandex.ru>
2012-04-08 22:51:14 +0700

    Update core/components/minishop/elements/snippets/hook_saveform.php

--- core/components/minishop/elements/snippets/hook_saveform.php
+++ core/components/minishop/elements/snippets/hook_saveform.php
@@ -2,6 +2,7 @@
 $data = $hook->getValues();
 
 if (!empty($data['delivery'])) {$_SESSION['minishop']['delivery'] = $data['delivery'];}
+if (!empty($data['payment'])) {$_SESSION['minishop']['payment'] = $data['payment'];}
 
 $_SESSION['minishop']['address'] = $data;
 
