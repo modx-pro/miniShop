edd38fbdc70f00b05db30aac653a6d34654d632c
bezumkin <bezumkin@yandex.ru>
2012-04-10 06:08:20 +0400

    Version 1.3.3

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -34,7 +34,7 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','miniShop');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('PKG_VERSION','1.3.2');
+define('PKG_VERSION','1.3.3');
 define('PKG_RELEASE','beta');
 
 /* define sources */
