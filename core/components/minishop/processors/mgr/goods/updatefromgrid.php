0d0bf64927e8d6faff80089950cf12fdf093f4c2
bezumkin <bezumkin@yandex.ru>
2012-05-01 01:10:57 +0700

    Returned /goods/updatefromgrid proccessor file

---
+++ core/components/minishop/processors/mgr/goods/updatefromgrid.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Update an Order from grid
+ * 
+ * @package minishop
+ * @subpackage processors
+ */
+
+if (!$modx->hasPermission('save')) {return $modx->error->failure($modx->lexicon('ms.no_permission'));}
+
+$tmp = $modx->fromJSON($scriptProperties['data']);
+$cid = $tmp['id'];
+$gid = $tmp['gid'];
+$enabled = $tmp['enabled'];
+
+if (empty($gid) || empty($cid)) {
+	return $modx->error->failure($modx->lexicon('ms.goods.err_ns'));
+}
+
+if ($res = $modx->getObject('ModCategories', array('gid' => $gid, 'cid' => $cid))) {
+	if (!$enabled) {
+		$res->remove();
+	}
+}
+else {
+	if ($enabled) {
+		$res = $modx->newObject('ModCategories', array('gid' => $gid, 'cid' => $cid));
+		$res->save();
+	}
+}
+
+/* output */
+return $modx->error->success('', '');
