43897fecff3bb644b00575c8a705906b896720a8
bezumkin <bezumkin@yandex.ru>
2012-05-03 08:34:33 +0400

    Improved duplicating of goods

--- core/components/minishop/processors/mgr/goods/duplicate.php
+++ core/components/minishop/processors/mgr/goods/duplicate.php
@@ -1,39 +1,38 @@
 <?php
 /**
- * miniShop
- *
- * Copyright 2010 by Shaun McCormick <shaun+minishop@modx.com>
- *
- * miniShop is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * miniShop is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * miniShop; if not, write to the Free Software Foundation, Inc., 59 Temple
- * Place, Suite 330, Boston, MA 02111-1307 USA
- *
- * @package minishop
- */
-/**
  * Duplicate an Goods
  * 
  * @package minishop
  * @subpackage processors
  */
-/* get board */
+ 
+
 if (!$modx->hasPermission('save')) {return $modx->error->failure($modx->lexicon('ms.no_permission'));}
 
-// Проверка обязательных полей
 if (empty($_POST['id'])) {
 	$modx->error->failure('pagetitle',$modx->lexicon('ms.required_field'));
 }
 
-$response = $modx->runProcessor('resource/duplicate', array('id' => $_POST['id'], 'name' => 'Copy of res #'.$_POST['id']));
+if ($res = $modx->getObject('modResource', $_POST['id'])) {
+	$name = $res->get('pagetitle') . ' copy';
+	if (strstr($name, ' copy #') != false) {
+		preg_match('/\#[\d]{1,3}/', $name, $tmp);
+		$i = substr($tmp[0], 1);
+		$name = preg_replace('/\scopy \#[\d]{1,3}/', '', $name);
+	}
+}
+
+if (!isset($i)) {$i = 1;}
+while (true) {
+	$add = ' #' . $i;
+	if ($modx->getCount('modResource', array('pagetitle' => $name.$add)) == 0) {
+		$name .= $add;
+		break;
+	}
+	$i++;
+}
+
+$response = $modx->runProcessor('resource/duplicate', array('id' => $_POST['id'], 'name' => $name));
 if ($response->isError()) {
     return $modx->error->failure($response->getMessage());
 }
@@ -43,6 +42,7 @@ $id_new = $response->response['object']['id'];
 
 if ($res_old = $modx->getObject('ModGoods', array('gid' => $id_old, 'wid' => $_SESSION['minishop']['warehouse']))) {
 	$tmp = $res_old->toArray();
+	$tags = $res_old->getTags();
 	unset($tmp['id']);
 	$tmp['gid'] = $id_new;
 
@@ -67,6 +67,7 @@ if ($res_old = $modx->getObject('ModGoods', array('gid' => $id_old, 'wid' => $_S
 		$tmp->set('gid', $id_new);
 		$tmp->save();
 	}
+	$res_new->addTags($tags);
 }
 else {
 	return $modx->error->failure($modx->lexicon('ms.goods.err_nf'));
