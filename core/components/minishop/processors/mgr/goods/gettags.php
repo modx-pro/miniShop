04f073b5baf0a302e7bf191d8d728c3ee48b6f82
bezumkin <bezumkin@yandex.ru>
2012-06-14 10:03:13 +0400

    Fixed adding new tags to goods

--- core/components/minishop/processors/mgr/goods/gettags.php
+++ core/components/minishop/processors/mgr/goods/gettags.php
@@ -5,41 +5,16 @@
  * @package minishop
  * @subpackage processors
  */
- 
 if (!$modx->hasPermission('view')) {return $modx->error->failure($modx->lexicon('ms.no_permission'));} 
 
-/*
-if (!isset($modx->miniShop) || !is_object($modx->miniShop)) {
-	$miniShop = $modx->getService('miniShop','miniShop',$modx->getOption('minishop.core_path',null,$modx->getOption('core_path').'components/minishop/').'model/minishop/', $scriptProperties);
-	if (!($miniShop instanceof miniShop)) return '';
-}
-
-$goods_tpls = $miniShop->config['ms_goods_tpls'];
-
-$c = $modx->newQuery('modTemplate');
-$c->where(array('modTemplate.id:IN' => $goods_tpls));
-$c->select('modTemplate.id,modTemplate.templatename');
-
-$count = $modx->getCount('modTemplate', $c);
-$res = $modx->getCollection('modTemplate',$c);
+$query = $modx->getOption('query', $_REQUEST, 0);
+$q = $modx->newQuery('ModTags');
 
 $arr = array();
-foreach ($res as $v) {
-	$tmp = array(
-		'id' => $v->get('id')
-		,'name' => $v->get('templatename')
-	);
-
-    $arr[]= $tmp;
-}
-*/
-
-$query = $modx->getOption('query', $_REQUEST, 0);
-
-$q = $modx->newQuery('ModTags');
 
 if (!empty($query)) {
 	$q->where(array('tag:LIKE' => '%'.$query.'%'));
+	$arr[] = array('tag' => $query);
 }
 $q->sortby('tag','ASC');
 $q->groupby('tag');
@@ -48,7 +23,7 @@ $count = $modx->getCount('ModTags', $q);
 $q->limit(10);
 
 $res = $modx->getCollection('ModTags', $q);
-$arr = array();
+
 foreach ($res as $v) {
     $arr[]= array(
 		'tag' => $v->get('tag')
