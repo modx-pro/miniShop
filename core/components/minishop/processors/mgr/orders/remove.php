f6810f51fd173286ff2757cc305a5e80d88e5e55
bezumkin <bezumkin@yandex.ru>
2012-07-16 06:58:02 +0400

    invokeEvent in delete processor

--- core/components/minishop/processors/mgr/orders/remove.php
+++ core/components/minishop/processors/mgr/orders/remove.php
@@ -8,19 +8,20 @@
 if (!$modx->hasPermission('remove')) {return $modx->error->failure($modx->lexicon('ms.no_permission'));}
 
 $id = $scriptProperties['id'];
-if (!$res = $modx->getObject('ModOrders', $id)) {
+if (!$order = $modx->getObject('ModOrders', $id)) {
 	return $modx->error->failure($modx->lexicon('ms.orders.err_nf'));
 }
 if ($modx->getOption('minishop.enable_remains')) {
-	$res->releaseReserved();
+	$order->releaseReserved();
 }
-
-if ($res->remove() == false) {
-    return $modx->error->failure($modx->lexicon('ms.orders.err_remove'));
-}
-else {
+$modx->invokeEvent('msOnBeforeOrderDelete', array('order' => $order));
+if ($order->remove()) {
 	$modx->removeCollection('ModOrderedGoods', array('oid' => $id));
 	$modx->removeCollection('ModLog', array('iid' => $id, 'type' => 'status'));
+	$modx->invokeEvent('msOnOrderDelete', array());
+}
+else {
+	return $modx->error->failure($modx->lexicon('ms.orders.err_remove'));
 }
 
-return $modx->error->success('',$res);
+return $modx->error->success('',$order);
